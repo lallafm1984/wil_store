1684be445e4913b2fd2bc07171384b32067a6881
lallafm1984 <lallafm1984@gmail.com>
2025-08-09 15:03:08 +0900

    08090303

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import LayoutContent from "@/components/LayoutContent";
+// import LayoutContent from "@/components/LayoutContent";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
